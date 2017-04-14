07799cf3bba3f0dff0406a90db6f97287e724f63
ryodocx <email@ryodocx.net>
2017-04-15 07:10:22 +0900

    Delete docker-compose.sh

--- ubuntu/docker-compose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-URL="https://github.com/docker/compose/releases/download/1.12.0/docker-compose-Linux-x86_64"
-INSTALL_DIR="/usr/local/bin"
-
-sudo wget $URL -O $INSTALL_DIR/docker-compose
-sudo chmod +x $INSTALL_DIR/docker-compose

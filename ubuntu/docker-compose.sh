76ed6c22aad26a3e146bac1987805ddbb955fe52
ryodocx <email@ryodocx.net>
2017-04-15 07:02:42 +0900

    Update docker-compose.sh

--- ubuntu/docker-compose.sh
+++ ubuntu/docker-compose.sh
@@ -3,5 +3,5 @@
 URL="https://github.com/docker/compose/releases/download/1.12.0/docker-compose-Linux-x86_64"
 INSTALL_DIR="/usr/local/bin"
 
-wget $URL -O $INSTALL_DIR/docker-compose
-chmod +x $INSTALL_DIR/docker-compose
+sudo wget $URL -O $INSTALL_DIR/docker-compose
+sudo chmod +x $INSTALL_DIR/docker-compose
